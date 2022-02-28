368e4ca78d84b95e50e429d18eeb30a34c0ec20a
Arda Erkmen <43019201+erkmenbyte2@users.noreply.github.com>
2022-02-28 20:22:44 +0300

    Create nesnelerle_temel_islemler.cpp

---
+++ nesnelerle_temel_islemler.cpp
@@ -0,0 +1,38 @@
+
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+class Deger 
+{
+public:
+	Deger() {
+		//s1 = rand() % 100;
+		//rastgale değer tuatn nami değer sınıf ımız biraz dan onu parçılacaz
+		cout<<this<<"adres de oluşturul dur"<<endl;
+	}
+	~Deger() 
+	{
+		cout<<this<<"adres de ki nesne püf oldu"<<endl;
+	}
+	void yazdr() {
+		cout<<s1<<"mevcut değer"<<endl;
+	}
+	private:
+		int s1;
+		
+	
+};
+int main(int argc, char** argv) {
+	
+	Deger * x[10];
+	
+	x[0] = new Deger;
+	x[1] = new Deger[1];
+	
+	x[0]->yazdr();
+	
+	
+	delete[] x;
+	
+	return 0;
+}
